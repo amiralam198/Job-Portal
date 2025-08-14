bb0e88cd796779582a5dd518abd81dea2a5bea74
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-08-14 20:39:26 +0530

    commit Hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,24 +5,35 @@ export default function Hero() {
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
       <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
-        <h2>Over 10,000+ jobs to apply</h2>
-        <p>Your next big Career Move starts Right Here-Explore The Best Job Opportunities And Take 
+        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
+        <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5'>Your next big Career Move starts Right Here-Explore The Best Job Opportunities And Take 
             The First Step Toward Your Future!</p>
-            <div>
-                <div>
-                    <img src={assets.search_icon} alt="" />
+            <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4  mx-4 sm:mx-auto'>
+                <div className='flex items-center'>
+                    <img className='h-4 sm:h-5' src={assets.search_icon} alt="" />
                     <input type="text"
                     placeholder='Search for Jobs'
                     className='max-sm:text-xs p-2 rounded outline-none w-full'/>
                 </div>
-                <div>
-                    <img src={assets.location_icon} alt="" />
+                <div className='flex items-center'>
+                    <img className='h-4 sm:h-5' src={assets.location_icon} alt="" />
                     <input type="text"
                     placeholder='Location'
                     className='max-sm:text-xs p-2 rounded outline-none w-full'/>
                 </div>
-                <button>Search</button>
+                <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
             </div>
+      </div>
+      <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+        <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
+          <p className='font-medium'> Trusted by</p>
+          <img className='h-6' src={assets.microsoft_logo}alt=""/>
+          <img className='h-6'src={assets.walmart_logo}alt=""/>
+          <img className='h-6'src={assets.accenture_logo}alt=""/>
+          <img className='h-6'src={assets.samsung_logo}alt=""/>
+          <img className='h-6'src={assets.amazon_logo}alt=""/>
+          <img className='h-6'src={assets.adobe_logo}alt=""/>
+        </div>
       </div>
     </div>
   )
