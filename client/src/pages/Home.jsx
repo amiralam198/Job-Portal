f494f79ce9fcfc20b47911d90b7f016d43f22579
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-08-15 16:53:39 +0530

    commit JobListing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import JobListing from "../components/JobListing";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Hero/>
-    </div>
+      <JobListing/>
+          </div>
   );
 }
