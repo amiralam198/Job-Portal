37f0bdf8fd0e41fd926332ca2f6ed5a0c822cbfd
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-08-16 20:24:16 +0530

    commit

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,17 +1,21 @@
-import React, { useContext,useState } from "react";
-import {
-  assets,
-  JobCategories,
-  JobLocations,
-} from "../assets/assets";
+import React, { useContext, useState } from "react";
+import { assets, JobCategories, JobLocations } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
-import  JobCard  from "../components/JobCard";
-
+import JobCard from "../components/JobCard";
 
 export default function JobListing() {
-  const { isSearched, searchFilter, setSearchFilter, jobs } = useContext(AppContext);
-  const [showFilter,setShowFilter] = useState(true)
-  const [currentPage,setCurrentPage] = useState(1);
+  const { isSearched, searchFilter, setSearchFilter, jobs } =
+    useContext(AppContext);
+  const [showFilter, setShowFilter] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedLocations, setSelectedLocations] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState(jobs);
+  const handleCategoryChange = (category) => {
+    setSelectedCategories(
+      prev => prev.includes(category) ? prev.filter(c => c !== category)
+    )
+  }
 
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
@@ -53,16 +57,19 @@ export default function JobListing() {
               </div>
             </>
           )}
-          <button onClick={e => setShowFilter(prev => !prev)} className="px-6 py-1.5 rounded border border-gray-400 lg:hidden">
-            {showFilter ? "Close" : "Filter"}
-          </button>
+        <button
+          onClick={(e) => setShowFilter((prev) => !prev)}
+          className="px-6 py-1.5 rounded border border-gray-400 lg:hidden"
+        >
+          {showFilter ? "Close" : "Filter"}
+        </button>
         {/* category filter */}
         <div className={showFilter ? "" : "max-lg:hidden"}>
           <h4 className="font-medium text-lg py-4">Search by Category</h4>
           <ul className="space-y-4 text-gray-600">
             {JobCategories.map((category, index) => (
               <li className="flex gap-3 items-center" key={index}>
-                <input className="scale-125" type="checkbox" name="" id="" />
+                <input className="scale-125" type="checkbox" onChange={} />
                 {category}
               </li>
             ))}
@@ -89,23 +96,46 @@ export default function JobListing() {
         </h3>
         <p className="mb-8">Get your desired job from top companies</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-          {jobs.slice((currentPage-1)*6,currentPage*6).map((job, index) => (
-            <JobCard key={index} job={job} />
-          ))}
+          {jobs
+            .slice((currentPage - 1) * 6, currentPage * 6)
+            .map((job, index) => (
+              <JobCard key={index} job={job} />
+            ))}
         </div>
         {/* pagination  */}
-        {jobs.length>0 && (
+        {jobs.length > 0 && (
           <div className="flex items-center justify-center space-x-2 mt-10">
             <a href="job-list">
-              <img onClick={() => setCurrentPage(Math.max(currentPage-1,1))} src={assets.left_arrow_icon}></img>
+              <img
+                onClick={() => setCurrentPage(Math.max(currentPage - 1, 1))}
+                src={assets.left_arrow_icon}
+              ></img>
             </a>
-            {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index)=>(
-              <a href="#job-list">
-                <button onClick={()=> setCurrentPage(index+1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage===index+1 ? 'bg-blue-100 text-blue-500' : 'text-gray-500' }`}>{index+1}</button>
-              </a>
-            ))}
+            {Array.from({ length: Math.ceil(jobs.length / 6) }).map(
+              (_, index) => (
+                <a href="#job-list">
+                  <button
+                    onClick={() => setCurrentPage(index + 1)}
+                    className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${
+                      currentPage === index + 1
+                        ? "bg-blue-100 text-blue-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    {index + 1}
+                  </button>
+                </a>
+              )
+            )}
             <a href="job-list">
-              <img onClick={() => setCurrentPage(Math.min(currentPage+1,Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon}></img>
+              <img
+                onClick={() =>
+                  setCurrentPage(
+                    Math.min(currentPage + 1, Math.ceil(jobs.length / 6))
+                  )
+                }
+                src={assets.right_arrow_icon}
+              ></img>
             </a>
           </div>
         )}
