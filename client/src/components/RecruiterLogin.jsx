e1ff814a8102f235cab92abde3a8caa30c966a1f
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2025-08-28 14:56:46 +0530

    commit RecruiterLogin PopUp

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 function RecruiterLogin() {
   const [state, setState] = useState("Login");
@@ -8,16 +9,38 @@ function RecruiterLogin() {
   const [email, setEmail] = useState("");
   const [image, setImage] = useState(false);
   const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
+  const {setShowRecruiterLogin} = useContext(AppContext)
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    if(state === 'Sign Up' && !isTextDataSubmitted){
+        setIsTextDataSubmitted(true)
+    }
+  }
+  // when pop occurs we can't scroll the webpage
+  useEffect(() =>{
+    document.body.style.overflow = 'hidden'
+    return () => {
+        document.body.style.overflow = 'unset' 
+    }
+  },[])
 
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+      <form onSubmit={onSubmitHandler}  className="relative bg-white p-10 rounded-xl text-slate-500">
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           Recruiter {state}
         </h1>
         <p className="text-sm">Welcome back! Please sign in to continue </p>
         {state === "Sign Up" && isTextDataSubmitted ? (
-          <></>
+          <>
+          <div className="flex items-center gap-4 my-10">
+            <label htmlFor="image">
+                <img className="w-16 rounded-full" src={image ? URL.createObjectURL(image): assets.upload_area} alt="" />
+                <input onChange={e=>setImage(e.target.files[0])} type="file" id="image" hidden />
+            </label>
+            <p>Upload Company <br/> logo</p>
+          </div>
+          </>
         ) : (
           <>
             {state !== "Login" && (
@@ -58,12 +81,12 @@ function RecruiterLogin() {
             </div>
           </>
         )}
-        <p className="text-sm text-blue-600 my-4 cursor-pointer">
+        {state==='Login' && <p className="text-sm text-blue-600 my-4 cursor-pointer">
           Forgot password
-        </p>
+        </p>}
 
-        <button className="bg-blue-600 w-full text-white py-2 rounded-full">
-          {state === "Login" ? "login" : "create account"}
+        <button type="submit" className="bg-blue-600 w-full text-white py-2 rounded-full mt-4">
+          {state === "Login" ? "login" : isTextDataSubmitted ? "create account" :'next'}
         </button>
         {state === "Login" ? (
           <p className="mt-5 text-center">
@@ -86,6 +109,7 @@ function RecruiterLogin() {
             </span>
           </p>
         )}
+        <img onClick={e => setShowRecruiterLogin(false)} className="absolute top-5 right-5 cursor-pointer" src={assets.cross_icon} alt="" />
       </form>
     </div>
   );
